44fd763e282a637f7b19a1a238b82c6e896afe3c
shibashiba48 <t1257476@gmail.com>
2020-05-22 23:53:16 +0900

    add-destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   has_many :tags, through: :posts_tags
   accepts_nested_attributes_for :tags, allow_destroy: true
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
