b65c91cec23ac030fd11b32bf2e1ff57300ab269
shibashiba48 <t1257476@gmail.com>
2020-06-09 23:37:17 +0900

    add-dependent

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :posts_tags
-  has_many :tags, through: :posts_tags
+  has_many :tags, dependent: :destroy, through: :posts_tags
   accepts_nested_attributes_for :tags, allow_destroy: true
   belongs_to :user
   has_many :images, dependent: :destroy
